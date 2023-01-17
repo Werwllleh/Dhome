09a1de6c6bf1306c825e1b6e5cee94d15ffb12c9
Werwllleh <allvxm4@gmail.com>
2023-01-17 18:13:20 +0300

    Start

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -5,7 +5,7 @@ const MainContainer = ({ children, title, keywords }) => {
 	return (
 		<>
 			<Head>
-				<link rel="icon" type="image/x-icon" href="public/favicon.ico" />
+				<link rel="icon" type="image/x-icon" href="/favicon.ico" />
 				<meta keywords={"next course, nextJS " + keywords}></meta>
 				<title>{title}</title>
 			</Head>
